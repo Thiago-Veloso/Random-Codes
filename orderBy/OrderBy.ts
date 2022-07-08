5350540b037f293aa09b04d906ba3efafd6312f1
Thiago Pio <thiago.pio2017@gmail.com>
2022-07-08 16:39:04 -0300

    Added decrescent method and refactory

--- orderBy/OrderBy.ts
+++ orderBy/OrderBy.ts
@@ -1,41 +1,73 @@
+type OrderType = "crescent" | "decrescent";
+
 // ---------------------------------------------------
-// Class for order array numbers
+// Class to sort array numbers
 // ---------------------------------------------------
 export class OrderBy {
-  static readonly exempleList = [1, 26, 57, -34, 0, -3] as number[];
+  // Ordes types vars
+  private static readonly CRESCENT = "crescent";
+  private static readonly DECRESCENT = "decrescent";
 
   constructor() {}
 
   /*
-   * Static Methods
+   * Private Methods
    */
 
-  static crescent(desordedList: number[] = this.exempleList): number[] {
-    // Array for orded numbers
-    const ordedList = [] as number[];
+  // Sort array according to typo
+  private static orderList(desorderedList: number[], type: OrderType) {
+    // Array for sorted numbers
+    const orderedList = [] as number[];
 
     // Iteration in list desorded
-    desordedList.forEach((desordedListNumber) => {
+    desorderedList.forEach((desorderedListNumber) => {
       // Add the first number
-      if (ordedList.length === 0) {
-        ordedList.push(desordedListNumber);
+      if (orderedList.length === 0) {
+        orderedList.push(desorderedListNumber);
         return;
       }
 
-      // Iterate in orded list for add the current number
-      ordedList.some((ordedListNumber, ordedListIndex) => {
-        // Check if the current number is smaller, if true, add in index
-        if (desordedListNumber <= ordedListNumber) {
-          ordedList.splice(ordedListIndex, 0, desordedListNumber);
+      // Iterate in sorted list for add the current number
+      orderedList.some((orderedListNumber, orderedListIndex) => {
+        // Check if the current number is smaller or bigger, if true, add in index
+        if (
+          this.checkTypeforOrder(desorderedListNumber, orderedListNumber, type)
+        ) {
+          orderedList.splice(orderedListIndex, 0, desorderedListNumber);
           return true;
         }
 
         // Checks if it is in the last index of the array, if true, add in the end
-        if (ordedList.length - 1 === ordedListIndex)
-          ordedList.push(desordedListNumber);
+        if (orderedList.length - 1 === orderedListIndex)
+          orderedList.push(desorderedListNumber);
       });
     });
 
-    return ordedList;
+    return orderedList;
+  }
+
+  // Check the type and perform the respective logic
+  private static checkTypeforOrder(
+    desorderedListNumber: number,
+    orderedListNumber: number,
+    typeOrder: OrderType
+  ): boolean {
+    return typeOrder === this.CRESCENT
+      ? desorderedListNumber <= orderedListNumber
+      : desorderedListNumber >= orderedListNumber;
+  }
+
+  /*
+   * Public Methods
+   */
+
+  // Returns an array arranged in descending order
+  static decrescent(desorderedList: number[]): number[] {
+    return this.orderList(desorderedList, this.DECRESCENT);
+  }
+
+  // returns an ascending array
+  static crescent(desorderedList: number[]): number[] {
+    return this.orderList(desorderedList, this.CRESCENT);
   }
 }
