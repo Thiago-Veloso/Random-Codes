f64107dbf16a9738eb7e80c5c261707f40999af1
Thiago Pio <thiago.pio2017@gmail.com>
2022-07-07 22:16:12 -0300

    first commit

---
+++ orderBy/OrderBy.ts
@@ -0,0 +1,43 @@
+// ---------------------------------------------------
+// Class for order array numbers
+// ---------------------------------------------------
+export class OrderBy {
+  static exempleList = [1, 26, 57, -34, 0, -3] as number[];
+
+  constructor() {}
+
+  /*
+   * Static Methods
+   */
+
+  static crescent(desordedList: number[] = this.exempleList): number[] {
+    // Array for orded numbers
+    const ordedList = [] as number[];
+
+    // Iteration in list desorded
+    desordedList.forEach((desordedListNumber) => {
+      // Add the first number
+      if (ordedList.length === 0) {
+        ordedList.push(desordedListNumber);
+        return;
+      }
+
+      // Iterate in orded list for add the current number
+      ordedList.some((ordedListNumber, ordedListIndex) => {
+        // Check if the current number is smaller, if true, add in index
+        if (desordedListNumber <= ordedListNumber) {
+          ordedList.splice(ordedListIndex, 0, desordedListNumber);
+          return true;
+        }
+
+        // checks if it is in the last index of the array, if true, add in the end
+        if (ordedList.length - 1 === ordedListIndex)
+          ordedList.push(desordedListNumber);
+      });
+    });
+
+    return ordedList;
+  }
+}
+
+console.log(OrderBy.crescent());
